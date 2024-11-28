87e9d9d0e0933614ca4782d5ec6a804d9d55c367
JanMajer86 <majer.jan.1986@gmail.com>
2024-11-28 16:01:14 +0100

    init

---
+++ routes/Bochanek.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const BochanekSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Bochánek musí mít jméno"],
+			maxlength: 60,
+		},
+		gender: {
+			type: String,
+			enum: ["male", "female"],
+			required: [true, "Bochánek musí mít gender"],
+		},
+		status: {
+			type: String,
+			enum: ["waiting", "approved", "unapproved"],
+			default: "waiting",
+		},
+		createdBy: {
+			type: mongoose.Types.ObjectId,
+			ref: "User",
+			required: [true, "Bochánka musí přidat uživatel"],
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Bochanek", BochanekSchema);
